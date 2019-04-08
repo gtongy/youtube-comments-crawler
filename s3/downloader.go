b6fd01930ed34630e1ecaf940e1e1c8d6e42d4a7
gtongy <32930281+gtongy@users.noreply.github.com>
2019-04-08 21:09:31 +0900

    add s3 client wrapeer

---
+++ s3/downloader.go
@@ -0,0 +1,42 @@
+package s3
+
+import (
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+type Downloader struct {
+	manager           s3manager.Downloader
+	key, bucket, dest string
+}
+
+func NewDownloader(key, bucket, dest string, session *session.Session) *Downloader {
+	return &Downloader{
+		manager: *s3manager.NewDownloader(session),
+		key:     key,
+		bucket:  bucket,
+		dest:    dest,
+	}
+}
+
+func (d *Downloader) Download() (string, error) {
+	file, err := os.Create(d.dest)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	numBytes, err := d.manager.Download(file, &s3.GetObjectInput{
+		Bucket: aws.String(d.bucket),
+		Key:    aws.String(d.key),
+	})
+	if err != nil {
+		return "", err
+	}
+	log.Print("Downloaded", file.Name(), numBytes, "bytes")
+	return file.Name(), nil
+}
