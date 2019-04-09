d421fabfecfc5431c6e7f8c752bc9eac5c97f101
gtongy <32930281+gtongy@users.noreply.github.com>
2019-04-09 21:02:46 +0900

    get caption id by video

--- handler/handler.go
+++ handler/handler.go
@@ -21,7 +21,12 @@ const (
 	// RequestSucessMessage は実行に成功時に返却するメッセージです
 	RequestSucessMessage = "実行に成功しました"
 	region               = endpoints.ApNortheast1RegionID
+	maxResult            = 3
 )
+
+type Video struct {
+	ID, Title string
+}
 
 func handleError(err error, message string) {
 	if message == "" {
@@ -32,16 +37,29 @@ func handleError(err error, message string) {
 	}
 }
 
-func channelsListByUsername(service *youtube.Service, part string, forUsername string) {
-	call := service.Channels.List(part)
-	call = call.ForUsername(forUsername)
+func getVideosById(service *youtube.Service, part string, id string) []Video {
+	call := service.Search.List(part)
+	call = call.ChannelId(id).MaxResults(maxResult).Order("date").Type("video")
 	response, err := call.Do()
 	handleError(err, "")
-	fmt.Println(fmt.Sprintf("This channel's ID is %s. Its title is '%s', "+
-		"and it has %d views.",
-		response.Items[0].Id,
-		response.Items[0].Snippet.Title,
-		response.Items[0].Statistics.ViewCount))
+	var videos []Video
+	for _, item := range response.Items {
+		videos = append(videos, Video{ID: item.Id.VideoId, Title: item.Snippet.Title})
+	}
+	return videos
+}
+
+func getCaptionIdsByVideos(service *youtube.Service, part string, videos []Video) []string {
+	var captionIds []string
+	for _, video := range videos {
+		call := service.Captions.List(part, video.ID)
+		response, err := call.Do()
+		handleError(err, "")
+		if response.Items != nil {
+			captionIds = append(captionIds, response.Items[0].Id)
+		}
+	}
+	return captionIds
 }
 
 // Handler はLambdaの処理実行ハンドラです
@@ -61,14 +79,16 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) (string, error) 
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
-	cfg, err := google.JWTConfigFromJSON(b, youtube.YoutubeReadonlyScope)
+	cfg, err := google.JWTConfigFromJSON(b, youtube.YoutubeForceSslScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
 	client := cfg.Client(context.Background())
 	service, err := youtube.New(client)
 	handleError(err, "Error creating YouTube client")
-	channelsListByUsername(service, "snippet,contentDetails,statistics", "GoogleDevelopers")
+	videos := getVideosById(service, "snippet", "UC5ry_Nn-9q-aCO0irGxRRsA")
+	captionIds := getCaptionIdsByVideos(service, "snippet", videos)
+	fmt.Println(captionIds)
 	return RequestSucessMessage, nil
 }
 
