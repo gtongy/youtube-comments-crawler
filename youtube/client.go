cef536f0e7c028eb885c51ee947954e69149f875
gtongy <32930281+gtongy@users.noreply.github.com>
2019-04-19 18:22:03 +0900

    youtube api return type change

--- youtube/client.go
+++ youtube/client.go
@@ -10,17 +10,9 @@ import (
 	"google.golang.org/api/youtube/v3"
 )
 
+// Client is youtube api wrapper
 type Client struct {
 	service *youtube.Service
-}
-
-func handleError(err error, message string) {
-	if message == "" {
-		message = "Error making API call"
-	}
-	if err != nil {
-		log.Fatalf(message+": %v", err.Error())
-	}
 }
 
 // NewClient is get client. this client is youtube client wrapper
@@ -35,34 +27,48 @@ func NewClient(secretFile []byte) Client {
 	return Client{service: service}
 }
 
+// VideoResponse is youtube api video response type
+type VideoResponse []model.Video
+
 // GetVideosIDsByChannelID is get video ids to use channel id
-// TODO: model.Video type is invalid
-func (c *Client) GetVideosIDsByChannelID(channelID string, maxResults int64) []model.Video {
+func (c *Client) GetVideosIDsByChannelID(channelID string, maxResults int64) VideoResponse {
 	call := c.service.Search.List("snippet")
 	call = call.ChannelId(channelID).MaxResults(maxResults).Order("date").Type("video")
 	response, err := call.Do()
 	handleError(err, "")
-	var videos []model.Video
+	var videoResponse VideoResponse
 	for _, item := range response.Items {
-		videos = append(videos, model.Video{ID: item.Id.VideoId, Title: item.Snippet.Title})
+		videoResponse = append(videoResponse, model.Video{ID: item.Id.VideoId, Title: item.Snippet.Title})
 	}
-	return videos
+	return videoResponse
 }
 
+// CommentResponse is youtube api comment response type
+type CommentResponse []model.Comment
+
 // GetCommentsByVideoID is get comments to use video id
-func (c *Client) GetCommentsByVideoID(videoID string, maxResults int64) []model.Comment {
+func (c *Client) GetCommentsByVideoID(videoID string, maxResults int64) CommentResponse {
 	call := c.service.CommentThreads.List("snippet")
 	call = call.VideoId(videoID).MaxResults(maxResults).TextFormat("plainText")
 	response, err := call.Do()
 	handleError(err, "")
-	var comments []model.Comment
+	var commentResponse CommentResponse
 	for _, item := range response.Items {
-		comments = append(comments, model.Comment{
+		commentResponse = append(commentResponse, model.Comment{
 			ID:   getXID(),
 			Text: item.Snippet.TopLevelComment.Snippet.TextDisplay,
 		})
 	}
-	return comments
+	return commentResponse
+}
+
+func handleError(err error, message string) {
+	if message == "" {
+		message = "Error making API call"
+	}
+	if err != nil {
+		log.Fatalf(message+": %v", err.Error())
+	}
 }
 
 func getXID() string {
