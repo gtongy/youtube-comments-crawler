1033d1c13c5d6e3ffa9f92d5ee88e5d3d4a2d7c2
gtongy <32930281+gtongy@users.noreply.github.com>
2019-04-29 17:02:30 +0900

    upload file name change, and api exec count exec from env

--- handler/handler.go
+++ handler/handler.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"time"
 
 	"encoding/json"
@@ -23,16 +24,20 @@ import (
 
 const (
 	region             = endpoints.ApNortheast1RegionID
-	maxVideosCount     = 1
-	maxCommentsCount   = 1
 	videosTableName    = "YoutubeCommentsCrawlerVideos"
 	commentsTableName  = "YoutubeCommentsCrawlerComments"
 	youtubersTableName = "YoutubeCommentsCrawlerYoutubers"
+	timeFormat         = "20060102150405"
 	dynamodbEndpoint   = "http://dynamodb:8000"
 	s3Endpoint         = "http://s3:9000"
 )
 
-// Handler ここで定義した関数内の処理を実行する
+var (
+	maxVideosCount, _   = strconv.ParseInt(os.Getenv("MAX_VIDEOS_COUNT"), 10, 64)
+	maxCommentsCount, _ = strconv.ParseInt(os.Getenv("MAX_COMMNETS_COUNT"), 10, 64)
+)
+
+// Handler ここで定義ßßした関数内の処理を実行する
 func Handler(ctx context.Context, event events.CloudWatchEvent) (string, error) {
 	s3Session := session.Must(session.NewSession(s3.Config(region, s3Endpoint)))
 	filename := serviceAccountFileDownload(s3Session)
@@ -49,21 +54,18 @@ func Handler(ctx context.Context, event events.CloudWatchEvent) (string, error) 
 
 	videoRepository := repository.Video{Table: db.Table(videosTableName)}
 	youtuberRepository := repository.Youtuber{Table: db.Table(youtubersTableName)}
-	uploader := s3.NewUploader(
-		os.Getenv("COMMENT_BUCKET"),
-		*s3manager.NewUploader(s3Session),
-	)
+	uploader := s3.NewUploader(os.Getenv("COMMENT_BUCKET"), *s3manager.NewUploader(s3Session))
+	currentTime := time.Now().Format(timeFormat)
 	for _, youtuber := range youtuberRepository.ScanAll() {
 		videos := youtubeClient.GetVideosIDsByChannelID(youtuber.ChannelID, maxVideosCount)
 		savedVideos := videoRepository.SaveAndGetVideos(videos)
-		t := time.Now().Format("20060102150405")
 		for _, savedVideo := range savedVideos {
 			comments := youtubeClient.GetCommentsByVideoID(savedVideo.ID, maxCommentsCount)
 			commentsJSON, err := json.Marshal(&comments)
 			if err != nil {
 				log.Fatalf("%v", err)
 			}
-			uploader.Upload(t+savedVideo.ID+".json", bytes.NewReader(commentsJSON))
+			uploader.Upload(currentTime+savedVideo.ID+".json", bytes.NewReader(commentsJSON))
 		}
 	}
 	return "success", nil
